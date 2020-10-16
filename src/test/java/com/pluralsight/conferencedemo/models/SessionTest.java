4af7ac8016e94857becd2a876b5e35a74ec09a7d
Olavo Brito Souza <olavo.souza@opus-software.com.br>
2020-10-16 11:11:11 -0300

    Add Aplication

---
+++ src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
@@ -0,0 +1,48 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.pluralsight.conferencedemo.repositories.SessionJpaRepository;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class SessionTest {
+    @Autowired
+    private SessionJpaRepository jpaRepository;
+
+    @Autowired
+    private SessionRepository repository;
+
+    @Test
+    public void test() throws Exception {
+        List<Session> sessions = repository.getSessionsThatHaveName("Java");
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testJpaNot() throws Exception {
+        List<Session> sessions = jpaRepository.findBySessionLengthNot(30);
+        assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testPagingSorting() throws Exception {
+        Page<Session> page = jpaRepository.getSessionsWithName("S", PageRequest.of(1, 5, Sort.by(Sort.Direction.DESC, "sessionLength")));
+        assertTrue(page.getTotalElements() > 0);
+    }
+
+    @Test
+    public void testCustomImpl() throws Exception {
+        List<Session> sessions = jpaRepository.customGetSessions();
+        assertTrue(sessions.size() > 0);
+    }
+
+}
